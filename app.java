4c7eeff14ad7a2ceee7c9f79efbc3491ef2c2fe2
renatovvjr <79455144+renatovvjr@users.noreply.github.com>
2021-09-24 15:58:44 -0300

    application
    Arquivo de programa

---
+++ app.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+/**
+ * Este Programa realiza o calculo fatorial de um número utilizando a
+ * recursividade.
+ */
+public class app {
+
+    public static int fatorial(int n) {
+        if (n <= 1)
+            return 1;
+        else
+            return n * fatorial(n - 1);
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int num1;
+        System.out.println("Informe o número do fatorial: ");
+        num1 = sc.nextInt();
+        System.out.println("O fatorial do número " + num1 + " é: " + fatorial(num1));
+
+    }
+
+}
